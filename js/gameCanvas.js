8554742126bf4d1467931ff2e8ac5cdb9c3e9a59
lempiy <lempiyada@gmail.com>
2016-07-08 00:07:52 +0300

    Init project with require.js - add canvas

---
+++ js/gameCanvas.js
@@ -0,0 +1,21 @@
+define(function(){
+
+    function GameCanvas() {
+        this.canvas = document.createElement('canvas'),
+        this.ctx = this.canvas.getContext('2d');
+        this.canvas.width = 1280;
+        this.canvas.height = 576;
+        this.canvas.id = 'my_canvas';
+    };
+
+    GameCanvas.prototype.setup = function() {
+        console.log(this)
+        document.body.appendChild(this.canvas);
+    };
+
+    var gameCanvas = new GameCanvas();
+
+    return {
+        gameCanvas: gameCanvas
+    }
+});
